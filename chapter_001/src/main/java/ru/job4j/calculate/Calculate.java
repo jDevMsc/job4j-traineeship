e84c18aa02800575325612aca3a22228e8eb49fe
Artem Luzan <unlimitet2011@yandex.ru>
2019-07-01 19:17:42 +0300

    1.4. JUnit[#154594]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,16 +1,27 @@
 package ru.job4j.calculate;
 
 /**
-*
-* Calculate
-*
-*/
-public class Calculate {   
-/**
-* @param args - args.
-*/
-   public static void main(String[] args) {
-	   
-      System.out.println("Hello, Job4j.");
-   }
+ * Calculate.
+ *
+ * @author Artem Luzan(artemmoscowpro@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Calculate {
+    /**
+     * Main.
+     * @param args - args.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+    /**
+     * Method echo.
+     * @param name Your name.
+     * @return Echo plus your name.
+     */
+    public String echo(String name) {
+        return "Echo, echo, echo : " + name;
+    }
 }
