a54491a9d557a3538a617c02b2d36bd37bc7622b
Artem Luzan <unlimitet2011@yandex.ru>
2019-07-02 20:36:36 +0300

    3.5. Стороны прямоугольника.[#154582]

---
+++ chapter_001/src/main/java/ru/job4j/condition/SqArea.java
@@ -0,0 +1,22 @@
+package ru.job4j.condition;
+
+/**
+ * Расчет площади прямоугольника
+ * @author Artem Luzan(artemmoscowpro@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class SqArea {
+    /**
+     * Расчет площади прямоугольника.
+     * @param p Периметр прямоугольника.
+     * @param k высота больше ширины в k раз.
+     * @return площадь.
+     */
+    public int square(int p, int k) {
+        int w = p/4*k;
+        int h = p/2-w;
+        int square =  w * h;
+        return square;
+    }
+}
