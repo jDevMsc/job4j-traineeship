33707a83d67788935d993e5455345d20bf014fcc
Artem Luzan <unlimitet2011@yandex.ru>
2019-07-02 19:44:10 +0300

    3.4. Расстояние между точками в системе координат[#154584]

---
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -0,0 +1,22 @@
+package ru.job4j.condition;
+
+/**
+ * Расчет расстояния между точками.
+ * @author Artem Luzan(artemmoscowpro@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Point {
+    /**
+     * Расчет расстояния между точками.
+     * @param x1 Коорджината X первой точки.
+     * @param x2 Коорджината X второй точки.
+     * @param y1 Коорджината Y первой точки.
+     * @param y2 Коорджината Y второй точки.
+     * @return расстояние.
+     */
+    public double distance(int x1, int y1, int x2, int y2) {
+        double result = Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
+        return result;
+    }
+}
