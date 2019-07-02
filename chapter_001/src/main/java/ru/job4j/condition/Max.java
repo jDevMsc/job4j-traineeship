023772a9432713383a3bdc4331f07d03244270af
Artem Luzan <unlimitet2011@yandex.ru>
2019-07-02 21:09:43 +0300

    4.2. Максимум из двух чисел[#154552]

---
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -0,0 +1,21 @@
+package ru.job4j.condition;
+
+/**
+ * @author Artem Luzan(artemmoscowpro@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class Max {
+
+    /**
+     * Возвращаем максимальное число.
+     * @param left Первое число для сравнения.
+     * @param right Второе число для сравнения.
+     * @return большее число.
+     */
+    public int max(int left, int right) {
+        int result = left>right? left : right;
+        return result;
+    }
+}
