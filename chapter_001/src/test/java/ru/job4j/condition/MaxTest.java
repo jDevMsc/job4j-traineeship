7a5ff24f69df977293b4f61dd8fd48beeee4a248
Artem Luzan <unlimitet2011@yandex.ru>
2019-07-02 21:12:24 +0300

    4.2. Максимум из двух чисел[#154552]

--- chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
@@ -11,4 +11,17 @@ public class MaxTest {
         int result = max.max(1, 2);
         assertThat(result, is(2));
     }
+    @Test
+    public void whenMax3To1Then3() {
+        Max max = new Max();
+        int result = max.max(3, 1);
+        assertThat(result, is(3));
+    }
+    @Test
+    public void when1And2ThenEquals() {
+        Max max = new Max();
+        int result = max.max(1, 1);
+        assertThat(result, is(1));
+    }
+
 }
