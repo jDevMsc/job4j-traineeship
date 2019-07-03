c0dce823a5851a61d0e741547491220e9461d45b
Artem Luzan <unlimitet2011@yandex.ru>
2019-07-03 14:10:14 +0300

    5.4. Протеиновая диета. [#156313]

--- chapter_001/src/main/java/ru/job4j/loop/Fitness.java
+++ chapter_001/src/main/java/ru/job4j/loop/Fitness.java
@@ -1,6 +1,17 @@
 package ru.job4j.loop;
-
+/**
+ * Программа по накачке.[#154561]
+ * @author Artem Luzan(artemmoscowpro@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
 public class Fitness {
+    /**
+     * Программа по накачке мышц за определенное кол-во месяцев.
+     * @param ivan тяга Ивана.
+     * @param nik тяга Николая.
+     * @return кол-во месяцев.
+     */
     public int calc(int ivan, int nik) {
         int month = 0;
         while (ivan < nik) {
