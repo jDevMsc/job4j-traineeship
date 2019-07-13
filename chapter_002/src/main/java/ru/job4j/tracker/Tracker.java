cfaa367d778f2ac8c1fc6e845c1f9ae7eeba9683
Artem Luzan <unlimitet2011@yandex.ru>
2019-07-13 12:26:39 +0300

    Реализовать  Tracker[#154669]

---
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -0,0 +1,29 @@
+package ru.job4j.tracker;
+
+public class Tracker {
+  private Item[] items = new Item[100];
+
+  public Item add(Item item){
+
+  }
+
+  public boolean replace(String id, Item item){
+
+  }
+
+  public boolean delete(String id){
+
+  }
+
+  public Item[] findAll(){
+
+  }
+
+  public Item[] findByName(String key){
+
+  }
+
+  public Item findById(String id){
+
+  }
+}
