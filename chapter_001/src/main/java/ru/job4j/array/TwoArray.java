19bc74abfee76726672314b18c8da0501a43a971
Artem Luzan <unlimitet2011@yandex.ru>
2019-07-15 12:02:25 +0300

    MergeArray

--- chapter_001/src/main/java/ru/job4j/array/TwoArray.java
+++ chapter_001/src/main/java/ru/job4j/array/TwoArray.java
@@ -2,10 +2,30 @@ package ru.job4j.array;
 
 public class TwoArray {
 
-    public int[]  merge(int[] left, int[] right) {
-        int[] thirdArray = new int[left.length + right.length];
-        System.arraycopy(left, 0, thirdArray, 0, left.length);
-        System.arraycopy(right, 0, thirdArray,  left.length, right.length);
-        return thirdArray;
+  public int[] merge(int[] left, int[] right) {
+    int[] thirdArray = new int[left.length + right.length];
+
+    int lInd = 0;
+    int rInd = 0;
+
+    for (int i = 0; i < thirdArray.length; i++) {
+      if (lInd == left.length) {
+        System.arraycopy(right, rInd, thirdArray, i, thirdArray.length - i);
+        break;
+      }
+      if (rInd == right.length) {
+        System.arraycopy(left, lInd, thirdArray, i, thirdArray.length - i);
+        break;
+      }
+
+      if (left[lInd] < right[rInd]) {
+        thirdArray[i] = left[lInd];
+        lInd++;
+      } else if (left[lInd] > right[rInd]) {
+        thirdArray[i] = right[rInd];
+        rInd++;
+      }
     }
+    return thirdArray;
+  }
 }
