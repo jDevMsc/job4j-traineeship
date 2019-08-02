dd59323dd1a64ce3fd8a0cc3bb292cfdef059210
Artem Luzan <unlimitet2011@yandex.ru>
2019-08-02 13:07:50 +0300

    Объединить два массиваv2

--- chapter_001/src/main/java/ru/job4j/array/TwoArray.java
+++ chapter_001/src/main/java/ru/job4j/array/TwoArray.java
@@ -1,29 +1,43 @@
 package ru.job4j.array;
-
+/**
+ * Соединить два массива
+ * @author Artem Luzan(artemmoscowpro@gmail.com)
+ */
 public class TwoArray {
-
+  /**
+   * Method соединяет два массива.
+   * @param left массив для работы.
+   * @param right массив для работы.
+   * @return измененный массив
+   */
   public int[] merge(int[] left, int[] right) {
     int[] thirdArray = new int[left.length + right.length];
 
     int lInd = 0;
     int rInd = 0;
 
-    for (int i = 0; i < thirdArray.length; i++) {
-      if (lInd == left.length) {
-        System.arraycopy(right, rInd, thirdArray, i, thirdArray.length - i);
-        break;
-      }
-      if (rInd == right.length) {
-        System.arraycopy(left, lInd, thirdArray, i, thirdArray.length - i);
-        break;
-      }
 
-      if (left[lInd] < right[rInd]) {
-        thirdArray[i] = left[lInd];
+
+    while (lInd < left.length && rInd < right.length) {
+
+      if ( left[lInd] < right[rInd]) {
+        thirdArray[lInd + rInd] = left[lInd];
         lInd++;
       } else if (left[lInd] > right[rInd]) {
-        thirdArray[i] = right[rInd];
+        thirdArray[lInd + rInd] = right[rInd];
         rInd++;
+      }
+
+      if (lInd >= left.length) {
+        while(rInd<right.length) {
+          thirdArray[lInd + rInd] = right[rInd];
+          rInd++;
+        }
+      } else if (rInd >= right.length) {
+        while(lInd<left.length) {
+          thirdArray[lInd + rInd] = left[lInd];
+          lInd++;
+        }
       }
     }
     return thirdArray;
